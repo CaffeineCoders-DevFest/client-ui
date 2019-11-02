8ba89d7ab6b6d5dade91420dd9f9ca759483b356
Casey Van Groll <caseyvangroll@gmail.com>
2019-11-02 12:07:42 -0500

    add react router, stub out homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import {Container, Row, Col, Jumbotron, Button} from 'react-bootstrap';
+import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Client UI
-      </header>
-    </div>
+    <Container>
+      <Jumbotron className='centered-children'>
+        <h1>Welcome!</h1>
+        <Row>
+          <Col xs={6}>
+            <Button variant="primary">Select a Technician by Type</Button>
+          </Col>
+          <Col xs={6}>
+            <Button variant="primary">Learn more</Button>
+          </Col>
+        </Row>
+      </Jumbotron>
+    </Container>
   );
 }
 
